6cba4d388a9eaf13d2b8890cb0c58fbaf212cc4e
Samson Umezulike <su881001@fh-muenster.de>
2019-01-29 03:04:33 +0100

    *Very* basic bot

---
+++ ref.py
@@ -0,0 +1,82 @@
+import configparser
+import discord
+from discord.ext import commands
+import sys
+import os
+import logging
+import logging.handlers
+
+
+CONFIG_FILE = "config/options.ini"
+
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+prefix = config["Chat"].get("CommandPrefix")
+auto_delete = config["Chat"].getboolean("DeleteMessages")
+debug_level = config["Chat"].get("DebugLevel")
+
+token = config["Credentials"].get("Token")
+description = config["Credentials"].get("Description")
+
+bot = commands.Bot(command_prefix=prefix,
+                   case_insensitive=True,
+                   pm_help=None,
+                   description=description,
+                   activity=discord.Game(name="ref!help"))
+
+
+async def get_oauth_url():
+    try:
+        data = await bot.application_info()
+    except Exception as e:
+        return "Couldn't retrieve invite link. Error: {}".format(e)
+    return discord.utils.oauth_url(data.id)
+
+
+@bot.event
+async def on_ready():
+    url = await get_oauth_url()
+    print(url)
+
+
+def set_logger():
+
+    if not os.path.exists("logs"):
+        print("Creating logs folder...")
+        os.makedirs("logs")
+
+    logger = logging.getLogger("referee")
+    logger.setLevel(logging.INFO)
+
+    ref_format = logging.Formatter(
+        '%(asctime)s %(levelname)s %(funcName)s %(lineno)d: '
+        '%(message)s',
+        datefmt="[%d/%m/%Y %H:%M]")
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(ref_format)
+
+    fhandler = logging.handlers.RotatingFileHandler(
+        filename='logs/ref.log', encoding='utf-8', mode='a',
+        maxBytes=10 ** 7, backupCount=5)
+    fhandler.setFormatter(ref_format)
+
+    logger.addHandler(fhandler)
+    logger.addHandler(stdout_handler)
+
+    dpy_logger = logging.getLogger("discord")
+
+    handler = logging.FileHandler(
+        filename='logs/discord.log', encoding='utf-8', mode='a')
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: '
+        '%(message)s',
+        datefmt="[%d/%m/%Y %H:%M]"))
+    dpy_logger.addHandler(handler)
+
+    return logger
+
+
+if __name__ == '__main__':
+    print("RUNNING REFEREE")
+    bot.run(token)
