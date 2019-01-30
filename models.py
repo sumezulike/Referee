340323426e176101efddb5fb094134cbc0db3a4d
Samson Umezulike <su881001@fh-muenster.de>
2019-01-30 23:16:35 +0100

    Added WarningObj, made changes to abstract class WarningDB, added tests for JSONWarningDB

--- models.py
+++ models.py
@@ -1,29 +1,63 @@
-import discord
+from __future__ import annotations
+from typing import List, Dict
 import abc
 import json
+import time
 import os
 
 
-class WarningDB(metaclass=abc.ABCMeta):
+class WarningObj:
+
+    def __init__(self, user_id: str, timestamp: float=time.time(), **kwargs):
+        self.user_id = user_id
+        self.timestamp = timestamp
+
+        self.message = kwargs.pop("message", "")
+        self.expiration_date = kwargs.pop("expiration_time", None)
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            return False
+        return self.__dict__ == other.__dict__
+
+    def get_remaining_time(self):
+        if not self.expiration_date:
+            return self.timestamp         # It's weird, I know
+
+        return self.expiration_date - time.time()
+
+    def is_expired(self):
+        return self.get_remaining_time() < 0
+
+
+class WarningDB(abc.ABC):
 
     @abc.abstractmethod
-    def __enter__(self):
+    def __enter__(self) -> WarningDB:
         pass
 
     @abc.abstractmethod
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
         pass
 
     @abc.abstractmethod
-    def get_warnings(self, user: discord.User) -> list:
+    def put_warning(self, warning: WarningObj) -> WarningObj:
         pass
 
     @abc.abstractmethod
-    def get_all_warnings(self) -> dict:
+    def get_warnings(self, user_id: str) -> List[WarningObj]:
         pass
 
     @abc.abstractmethod
-    def delete_warnings(self, user: discord.User) -> int:
+    def get_all_warnings(self) -> Dict[str, List[WarningObj]]:
+        pass
+
+    @abc.abstractmethod
+    def delete_warnings(self, user_id: str) -> int:
+        pass
+
+    @abc.abstractmethod
+    def delete_warning(self, warning: WarningObj):
         pass
 
     @abc.abstractmethod
@@ -31,40 +65,66 @@ class WarningDB(metaclass=abc.ABCMeta):
         pass
 
 
+def serialize_warning(warning: WarningObj) -> list:
+    return [warning.user_id, warning.timestamp, warning.message, warning.expiration_date]
+
+
+def deserialize_warning(s_warning: list) -> WarningObj:
+    return WarningObj(user_id=s_warning[0], timestamp=s_warning[1], message=s_warning[2], expiration_date=s_warning[3])
+
+
 class JSONWarningDB(WarningDB):
 
-    def __init__(self, filepath: str, **kwargs):
+    def __init__(self, filepath: str):
         self.filepath = filepath
-        self.file = None
-        self.data = {}
+        self.data: Dict[str, List[list]] = {}
 
     def __enter__(self):
         if not os.path.exists(self.filepath):
             with open(self.filepath, "w") as file:
                 file.write("{}")
-        self.file = open(self.filepath, "w")
-        self.data = json.load(self.file)
+                self.data = {}
+        else:
+            with open(self.filepath, "r") as file:
+                content = file.read()
+                if len(content) == 0:
+                    self.data = {}
+                else:
+                    self.data = json.loads(content)
+
         if not isinstance(self.data, dict):
-            raise RuntimeError("Invalid form")
+            raise RuntimeError("Malformed database")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        json.dump(self.data, self.file)
-        self.file.close()
+        with open(self.filepath, "w") as file:
+            json.dump(self.data, file)
+        return True
 
-    def get_warnings(self, user: discord.User) -> list:
-        return self.data.get(user.id, [])
+    def put_warning(self, warning: WarningObj) -> WarningObj:
+        s = serialize_warning(warning)
+        if warning.user_id in self.data.keys():
+            self.data[warning.user_id].append(s)
+        else:
+            self.data[warning.user_id] = [s]
+        return deserialize_warning(s)
 
-    def get_all_warnings(self) -> dict:
-        return self.data
+    def get_warnings(self, user_id: str) -> List[WarningObj]:
+        return [deserialize_warning(w) for w in self.data.get(user_id, [])]
 
-    def delete_warnings(self, user: discord.User) -> int:
-        count = len(self.data.get(user.id, []))
-        self.data[user.id] = []
+    def get_all_warnings(self) -> Dict[str, List[WarningObj]]:
+        return {user_id: [deserialize_warning(w) for w in w_list] for user_id, w_list in self.data.items()}
+
+    def delete_warning(self, warning: WarningObj):
+        # TODO: implement
+        pass
+
+    def delete_warnings(self, user_id: str) -> int:
+        count = len(self.data.get(user_id, []))
+        self.data[user_id] = []
         return count
 
     def delete_all_warnings(self) -> int:
         count = sum([len(x) for x in self.data.values()])
         self.data = {}
         return count
-
---
+++ test_JSONWarningDB.py
@@ -0,0 +1,53 @@
+import unittest
+from models import JSONWarningDB, WarningObj
+import time
+
+DB_FILE = "tests.json"
+
+
+class TestJSONWarningDB(unittest.TestCase):
+
+    def setUp(self):
+        with open(DB_FILE, "w") as file:
+            file.write("{}")
+
+    def test_with(self):
+        with JSONWarningDB(filepath=DB_FILE) as db:
+            pass
+
+    def test_put_warning_return(self):
+        user_id = "123456789"
+        message = "Hello. My name is Inigo Montoya."
+
+        warning = WarningObj(user_id=user_id, message=message)
+        with JSONWarningDB(filepath=DB_FILE) as db:
+            re_warning = db.put_warning(warning)
+            self.assertEqual(warning, re_warning)
+
+    def test_put_warning_get_warning(self):
+        user_id = "123456789"
+        message = "Hello. My name is Inigo Montoya."
+
+        warning = WarningObj(user_id=user_id, message=message)
+        with JSONWarningDB(filepath=DB_FILE) as db:
+            db.put_warning(warning)
+            re_warning = db.get_warnings(user_id=user_id)[0]
+        self.assertEqual(warning, re_warning)
+
+
+    def test_put_warning_get_warning_persistence(self):
+        user_id = "123456789"
+        message = "Hello. My name is Inigo Montoya."
+
+        warning = WarningObj(user_id=user_id, message=message)
+        with JSONWarningDB(filepath=DB_FILE) as db:
+            db.put_warning(warning)
+
+        with JSONWarningDB(filepath=DB_FILE) as db:
+            re_warnings = db.get_warnings(user_id=user_id)
+        self.assertTrue(re_warnings)
+        self.assertEqual(warning, re_warnings[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
