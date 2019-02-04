f439d431308edcbb8f02ce7f135320370eee468a
Samson Umezulike <su881001@fh-muenster.de>
2019-02-04 14:18:58 +0100

    Wow, that was stupid

--- ref.py
+++ ref.py
@@ -216,12 +216,15 @@ def set_logger() -> logging.Logger:
 
 
 def get_warned_color(color: tuple) -> tuple:
+    def is_grey(c):
+        return max([abs(c[0]-c[1]), abs(c[1]-c[2]), abs(c[0]-c[2])]) < 10
+
     new_color = (color[0] // 2, color[1] // 2, color[2] // 2)
     default_warned_color = (120, 100, 100)
-    if sum(new_color)/3 < 60:
+    if sum(new_color)/3 < 60 and is_grey(new_color):
         return default_warned_color
     else:
-        return default_warned_color
+        return new_color
 
 
 def remove_formatting(text: str) -> str:
