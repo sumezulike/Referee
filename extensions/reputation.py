09fc72b49e666609c1ab303d33e78b7e8fbb443c
Samson Umezulike <umezulike@fh-muenster.de>
2020-05-24 22:03:20 +0200

    Added month and week option to leaderboard, changed rank progression, added rank to rep command

--- extensions/reputation.py
+++ extensions/reputation.py
@@ -70,61 +70,104 @@ class Reputation(commands.Cog):
                             except Exception as e:
                                 logger.error(e)
                             await message.add_reaction(emoji.thumbs_up)
-                await message.delete(delay=30)
 
 
     @commands.command(name="rep", aliases=["get_rep"])
     async def get_rep(self, ctx: commands.Context, member: discord.Member = None):
         if not member:
             member = ctx.author
+        leaderboard = await self.db.get_leaderboard()
+
+        ranks = {score: i+1 for i, score in enumerate(sorted(set(x["current_rep"] for x in leaderboard), reverse=True))}
+
+        rep = await self.db.get_user_rep(member.id)
         embed = discord.Embed(title="Reputation", color=discord.Color.dark_gold())
         embed.add_field(name=f"{member.name}'s reputation:",
-                        value=str(await self.db.get_user_rep(member.id)),
+                        value=f"{rep} (Rank #{ranks.get(rep, len(ranks)+1)})",
                         inline=True)
         await ctx.send(embed=embed)
 
 
     @commands.group(name="leaderboard")
     async def leaderboard(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
 
-        embed = discord.Embed(title="Leaderboard", color=discord.Color.dark_gold())
-        i = 0
-        embed.add_field(
-            name="Highest Reputations Scores:",
-            value="".join(
-                "{}: {}#{}: {}\n".format(
-                    str(i := i + 1).zfill(len(str(reputation_config.Leader_Limit))),
-                    self.bot.get_user(x['user_id']).name,
-                    self.bot.get_user(x['user_id']).discriminator, x['current_rep']
-                )
-                for x in await self.db.get_leaderboard()))
-        await ctx.send(embed=embed)
+            leaderboard = await self.db.get_leaderboard()
+            ranks = {score: i + 1 for i, score in
+                     enumerate(sorted(set(x["current_rep"] for x in leaderboard), reverse=True))}
+
+            embed = discord.Embed(title="Leaderboard", color=discord.Color.dark_gold())
+            embed.add_field(
+                name="Highest Reputations Scores:",
+                value="\n".join(
+                    "{}: {}#{}: {}".format(
+                        str(ranks.get(x["current_rep"])).zfill(len(str(reputation_config.Leader_Limit))),
+                        self.bot.get_user(x['user_id']).name,
+                        self.bot.get_user(x['user_id']).discriminator, x['current_rep']
+                    )
+                    for x in leaderboard))
+            await ctx.send(embed=embed)
 
 
     @leaderboard.command()
-    async def month(self, ctx: commands.Context, month_number: int = None):
-        month_name = datetime.strptime(month_number, "%m").strftime("%B")
+    async def month(self, ctx: commands.Context, month_number: int = date.today().month):
+
+        month_name = datetime.strptime(str(month_number), "%m").strftime("%B")
 
         since = date(date.today().year, month_number, 1)
         until = since + timedelta(days=30)
 
-        score = {}
+        member_scores = {}
         for res in await self.db.get_thanks_timeframe(since, until):
-            score[res["target_user"]] = score.get(res["target_user"], 0) + 1
+            member_scores[res["target_user"]] = member_scores.get(res["target_user"], 0) + 1
 
-        leaderboard = sorted(score, key=score.get, reverse=True)
+        leaderboard = sorted(member_scores, key=member_scores.get, reverse=True)
+        ranks = {score: i + 1 for i, score in
+                 enumerate(sorted(set(member_scores.values()), reverse=True))}
+
+        if not leaderboard:
+            embed = discord.Embed(title=f"No entries for {month_name}", color=discord.Color.dark_gold())
+        else:
+            embed = discord.Embed(title="Leaderboard", color=discord.Color.dark_gold())
+            i = 0
+            embed.add_field(
+                name=f"Highest Reputations Scores for {month_name}:",
+                value="\n".join(
+                    "{}: {}#{}: {}".format(
+                        str(ranks.get(member_scores.get(user_id))).zfill(len(str(reputation_config.Leader_Limit))),
+                        self.bot.get_user(user_id).name,
+                        self.bot.get_user(user_id).discriminator, member_scores.get(user_id)
+                    )
+                    for user_id in leaderboard))
+        await ctx.send(embed=embed)
+
+
+    @leaderboard.command()
+    async def week(self, ctx: commands.Context):
+
+        until = date.today() + timedelta(days=1)
+        since = until - timedelta(days=8)
+
+        member_scores = {}
+        for res in await self.db.get_thanks_timeframe(since, until):
+            member_scores[res["target_user"]] = member_scores.get(res["target_user"], 0) + 1
+
+        leaderboard = sorted(member_scores, key=member_scores.get, reverse=True)
+        ranks = {score: i + 1 for i, score in
+                 enumerate(sorted(set(member_scores.values()), reverse=True))}
+
 
         embed = discord.Embed(title="Leaderboard", color=discord.Color.dark_gold())
         i = 0
         embed.add_field(
-            name=f"Highest Reputations Scores for {month_name}:",
-            value="".join(
-                "{}: {}#{}: {}\n".format(
-                    str(i := i + 1).zfill(len(str(reputation_config.Leader_Limit))),
-                    self.bot.get_user(key).name,
-                    self.bot.get_user(key).discriminator, score.get(key)
+            name=f"Highest Reputations Scores for previous 7 days:",
+            value="\n".join(
+                "{}: {}#{}: {}".format(
+                    str(ranks.get(member_scores.get(user_id))).zfill(len(str(reputation_config.Leader_Limit))),
+                    self.bot.get_user(user_id).name,
+                    self.bot.get_user(user_id).discriminator, member_scores.get(user_id)
                 )
-                for key in leaderboard))
+                for user_id in leaderboard))
         await ctx.send(embed=embed)
 
 
